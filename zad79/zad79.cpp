e1d98a177133f1622f009ec905f97ad897c66e1d
Filipstrozik <47420153+Filipstrozik@users.noreply.github.com>
2020-02-02 16:09:27 +0100

    Add files via upload

--- zad79/zad79.cpp
+++ zad79/zad79.cpp
@@ -2,9 +2,10 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <cmath>
 using namespace std;
 
-struct okrag
+struct okrag //nazwy structur duza litera
 {
 double x;
 double y;
@@ -15,12 +16,12 @@ double r;
 vector<okrag> wczytaj (const string& nazwa)
 {
     vector<okrag> okregi;
-    okrag temp;
     ifstream input(nazwa);
     if(input.is_open())
     {
         for(int i=0;i<2000;i++)
         {
+            okrag temp;
             input>>temp.x;
             input>>temp.y;
             input>>temp.r;
@@ -110,13 +111,114 @@ vector<string> zad1 (const vector<okrag> okregi)
 
     return odp;
 }
+bool czy_odbicie (const okrag& o1,const okrag& o2)
+{
+    if(o1.r==o2.r)
+    {
+        if(o1.x == o2.x and o1.y == -o2.y or o1.x ==-o2.x and o1.y == o2.y)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+string zad2 (const vector<okrag>& okregi)
+{
+    int pary=0;
+    for(int i=1;i<okregi.size();i++)
+    {
+        for(int l=0;l<i;l++)
+        {
+            if(czy_odbicie(okregi[i],okregi[l]))
+            {
+                pary++;
+            }
+        }
+    }
+    return "zad2: liczba lustrzanych par: " + to_string(pary);
+}
+
+bool czy_prostopadle(const okrag& o1, const okrag& o2)
+{
+    if(o1.r==o2.r)
+    {
+        if(o1.x == o2.y and o1.y == -o2.x or o1.x ==-o2.y and o1.y == o2.x)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+string zad3 (const vector<okrag>& okregi)
+{
+    int pary_90=0;
+    for(int i=1;i<okregi.size();i++)
+    {
+        for(int l=0;l<i;l++)
+        {
+            if(czy_prostopadle(okregi[i],okregi[l]))
+            {
+                    pary_90++;
+            }
+        }
+    }
+    return "zad3: liczba prostopadlych par: " + to_string(pary_90);
+}
+bool czy_przeciecie(const okrag& o1, const okrag& o2)
+{
+    double s1s2 = sqrt(pow((o2.x-o1.x),2)+pow(o2.y-o1.y,2));
+    double ro = abs(o1.r-o2.r);
+    double so = o1.r+o2.r;
+    if(ro<=s1s2 and s1s2<=so)
+    {
+        return true;
+    }
+    return false;
+}
+
+vector<string> zad4 (const vector<okrag>& okregi)
+{
+    vector<string> odp;
+    int max_dl=0;
+    int dl_lancuch=1;
+    for(int i=0; i<999 ;i++)
+    {
+        if(czy_przeciecie(okregi[i],okregi[i+1]))
+        {
+            dl_lancuch++;
+            if(i==998)
+            {
+                odp.push_back("dlugosc kojenego lancucha: " + to_string(dl_lancuch));
+            }
+        }
+        else
+        {
+            odp.push_back("dlugosc kojenego lancucha: " + to_string(dl_lancuch));
+            dl_lancuch=1;
+        }
+        if(max_dl<dl_lancuch)
+        {
+            max_dl=dl_lancuch;
+        }
+    }
+    odp.push_back("dlugosc najdluzszego lancucha: " + to_string(max_dl));
+    return odp;
+
+}
 
 int main ()
 {
     vector<string> odp;
     vector<okrag> dane = wczytaj("okregi.txt");
     vector<string> zad1_odp = zad1(dane);
+    string zad2_odp = zad2(dane);
+    string zad3_odp = zad3(dane);
+    vector<string> zad4_odp = zad4(dane);
     odp.insert(odp.end(),zad1_odp.begin(),zad1_odp.end());
+    odp.push_back(zad2_odp);
+    odp.push_back(zad3_odp);
+    odp.insert(odp.end(),zad4_odp.begin(),zad4_odp.end());
     zapisz("wyniki.txt",odp);
 
 
