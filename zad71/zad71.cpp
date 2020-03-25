5f02a346c557f6be169dfe49802fc6fe47f3309a
adje271782 <extern.adrian.jekiel@elektrobit.com>
2020-03-25 19:16:40 +0100

    setPrecision example

--- zad71/zad71.cpp
+++ zad71/zad71.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <algorithm>
 #include <iomanip>
+#include <sstream>
+
 using namespace std;
 
 vector<vector<double>> wczytaj (const string& name)
@@ -44,13 +46,7 @@ void zapisz (const string& name, const vector<string>& odp)
 }
 double fun (const vector<double>& wspol ,const double& x)
 {
-    double funkcja;
-    double a0 = wspol[0];
-    double a1 = wspol[1];
-    double a2 = wspol[2];
-    double a3 = wspol[3];
-    funkcja = a0 + (a1*x) +(a2*(pow(x,2))) + (a3*(pow(x,3)));
-    return funkcja;
+    return wspol[0] + (wspol[1]*x) +(wspol[2]*(pow(x,2))) + (wspol[3]*(pow(x,3)));
 }
 
 double funkcja (double x, vector<vector<double>> wspolczynniki)
@@ -76,43 +72,35 @@ double funkcja (double x, vector<vector<double>> wspolczynniki)
         return fun(wspolczynniki[4],x);
     }
 }
-double zaokragli (const int& dokladnosc, const double& liczba)
-{
-    double wynik = liczba;
-    wynik = round(wynik*pow(double(10),dokladnosc))/pow(double(10),dokladnosc);
-    return wynik;
-}
 
-vector<string> zad2 (vector<vector<double>> wspolczynniki)
+string zad2 (vector<vector<double>> wspolczynniki)
 {
-    vector<string> odp;
+    stringstream odp;
     double wynik_maks=0;
     double argument=0;
     for(double x=4.0;x<5.0;x+=0.001)
     {
-        double wynik= zaokragli(5,funkcja(x,wspolczynniki));
+        double wynik= funkcja(x,wspolczynniki);
         if(wynik_maks<wynik)
         {
             argument=x;
             wynik_maks=wynik;
         }
     }
-    odp.push_back("zad2:");
-    odp.push_back("x="+to_string(argument));
-    odp.push_back("f(x)=" +to_string(wynik_maks));
-    return odp;
+    odp << "Zad 2:" << endl;
+    odp << "x= " << setprecision(4) << argument << endl;
+    odp << "f(x)= " << setprecision(6) << wynik_maks << endl;
+    return odp.str();
 }
+
 double bisekcja(double left,double right,const vector<vector<double>>& wspolczynniki)
 {
     double midx = ((right-left)/2.0)+left;
 
     double fl = funkcja(left, wspolczynniki);
     double fmid = funkcja(midx, wspolczynniki);
-    double fr = funkcja(right, wspolczynniki);
-    cout<<midx<<" "<<fmid<<" "<<fl<<" "<<fr<<endl;
     if(abs(fmid)<0.00001)
     {
-        cout<<"zwracam"<<endl;
         return ((right-left)/2.0)+left;
     }
     else
@@ -128,28 +116,29 @@ double bisekcja(double left,double right,const vector<vector<double>>& wspolczyn
     }
 }
 
-vector<string> zad3 (vector<vector<double>> wspolczynniki)
+string zad3 (const vector<vector<double>>& wspolczynniki)
 {
-    double m0 = bisekcja(0.0,1.0,wspolczynniki);
-    //double m1 = bisekcja(2.0,3.0,wspolczynniki);
-    //double m2 = bisekcja(3.0,4.0,wspolczynniki);
-    //double m3 = bisekcja(4.0,5.0,wspolczynniki);
-    cout<<m0<<endl;
-    //cout<<m1<<endl;
-    //cout<<m2<<endl;
-    //cout<<m3<<endl;
+    stringstream odp;
+    odp << "Zad3:" << endl;
+    odp << "m[0] : " << bisekcja(0.0,1.0,wspolczynniki) << endl;
+    odp << "m[1] : " << bisekcja(2.0,3.0,wspolczynniki) << endl;
+    odp << "m[2] : " << bisekcja(3.0,4.0,wspolczynniki) << endl;
+    odp << "m[3] : " << bisekcja(4.0,5.0,wspolczynniki) << endl;
+    return odp.str();
+}
+
+string zad1(const vector<vector<double>>& wspolczynniki)
+{
+    stringstream odp;
+    odp << "Zad1:" << endl;
+    odp << "f(1.5) = " << setprecision(6) << funkcja(1.5,wspolczynniki) << endl;
+    return odp.str();
 }
 
 
 int main ()
 {
     vector<vector<double>> wspolczynniki = wczytaj("funkcja.txt");
-    vector<string> odp;
-    vector<string> z2 = zad2(wspolczynniki);
-    vector<string> z3 = zad3(wspolczynniki);
-    odp.push_back("zad1:");
-    odp.push_back("f(1.5)=" + to_string(zaokragli(5,funkcja(1.5,wspolczynniki))));
-    odp.insert(odp.end(),z2.begin(),z2.end());
+    vector<string> odp{zad1(wspolczynniki), zad2(wspolczynniki), zad3(wspolczynniki)};
     zapisz("wyniki.txt",odp);
-
 }
