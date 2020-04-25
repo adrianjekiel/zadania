330580190290e2599a7c0c36ada57a870fc5ef2b
Filipstrozik <47420153+Filipstrozik@users.noreply.github.com>
2020-04-25 13:17:29 +0200

    Add files via upload

---
+++ zad62/zad62.cpp
@@ -0,0 +1,137 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+using namespace std;
+
+
+vector<int> wczytaj(const string& nazwa)
+{
+    vector<int> liczby;
+    int temp;
+    ifstream input(nazwa);
+    if(input.is_open())
+    {
+        while(input>>temp)
+        {
+            liczby.push_back(temp);
+        }
+    }
+    input.close();
+    return liczby;
+}
+
+vector<int> wczytaj2(const string& nazwa)
+{
+    vector<int> liczby;
+    int temp;
+    ifstream input(nazwa);
+    if(input.is_open())
+    {
+        while(input>>oct>>temp)
+        {
+            liczby.push_back(temp);
+        }
+    }
+    input.close();
+    return liczby;
+}
+
+void zapisz (const string& nazwa, const vector<string> odp)
+{
+    string temp;
+    ofstream output(nazwa);
+    if(output.is_open())
+    {
+        for(auto s: odp)
+        {
+            output << s <<endl;
+        }
+    }
+    output.close();
+}
+string zad1(const vector<int>& liczby)
+{
+    stringstream odp;
+    odp << "zad1: ";
+    int min_l=liczby[0];
+    int max_l=liczby[0];
+    for(const auto& liczba:liczby)
+    {
+        if(liczba>max_l)
+        {
+            max_l=liczba;
+        }
+        if(liczba<min_l)
+        {
+            min_l=liczba;
+        }
+    }
+    odp << "najmniejsza liczba : " << min_l << " maksymalna liczba: "<< max_l;
+    return odp.str();
+}
+
+string zad2 (const vector<int>& liczby)
+{
+    stringstream odp;
+    odp << "zad2: ";
+    vector<int> temp,maks;
+    for(int i=0;i<liczby.size()-1;i++)
+    {
+        temp.push_back(liczby[i]);
+        if(liczby[i]>liczby[i+1])
+        {
+            if(maks.size()<temp.size())
+            {
+                maks=temp;
+            }
+            temp.clear();
+        }
+    }
+    odp << maks[0] << " "<< maks.size();
+    return odp.str();
+}
+
+int convert_to_dec (long long liczba,int podstawa_sys)
+{
+    int dec_val = 0;
+    int base = 1;
+    long long temp = liczba;
+    while (temp)
+    {
+     long long last= temp%10;
+     temp = temp/10;
+     dec_val += last * base;
+     base = base *podstawa_sys;
+    }
+    return dec_val;
+}
+
+string zad3(const vector<int>& osemkowe,const vector<int>& dziesietne)
+{
+    int licznik_a=0;
+    int licznik_b=0;
+    stringstream odp;
+    odp <<"zad3: ";
+    for(int i=0;i<osemkowe.size();i++)
+    {
+        if(osemkowe[i]==dziesietne[i])
+        {
+            licznik_a++;
+        }
+        if(osemkowe[i]>dziesietne[i])
+        {
+            licznik_b++;
+        }
+    }
+    odp <<"odp a): "<<licznik_a <<"\n odp b): " << licznik_b;
+    return odp.str();
+}
+
+main()
+{
+    auto dane_osemkowe = wczytaj2("liczby1.txt");
+    auto dane_dziesietne = wczytaj("liczby2.txt");
+    zapisz("wyniki.txt",{zad1(dane_osemkowe),zad2(dane_dziesietne),zad3(dane_osemkowe,dane_dziesietne)});
+}
