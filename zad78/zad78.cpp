8a8825b76cc424373dfc19f4f8b14528855a05a9
Filipstrozik <47420153+Filipstrozik@users.noreply.github.com>
2020-06-05 17:15:33 +0200

    Add files via upload

--- zad78/zad78.cpp
+++ zad78/zad78.cpp
@@ -45,10 +45,11 @@ string skrot(string wiadomosc)
     {
         wiadomosc.push_back('.');
     }
-    int j=0;
+
     int i=0;
     while(i<wiadomosc.size())
     {
+        int j=0;
         while(j<8)
         {
             S[j]=S[j]+(wiadomosc[i]%128);
@@ -58,10 +59,12 @@ string skrot(string wiadomosc)
             i++;
         }
     }
+    cout<<S;
     for(char& znak:S)
     {
         znak = 65+znak%26;
     }
+    cout<<S;
     return S;
 }
 
@@ -80,6 +83,6 @@ void zapisz (const string& nazwa, const vector<string> odp)
 }
 main()
 {
-    vector<Wiad_podpis> dane = wczytaj("wiadomosc.txt","podpis.txt");
+    vector<Wiad_podpis> dane = wczytaj("wiadomosci.txt","podpisy.txt");
     zapisz("wyniki.txt",{skrot(dane[0].wiad)});
 }
